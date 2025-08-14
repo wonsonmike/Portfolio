32d1297bfffee720642ac3b1db4f20385c7510e0
mikewonson <mwonsoncloud@gmail.com>
2025-08-14 12:49:29 +0300

    working on mobile nav

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,10 +1,15 @@
 const nav = document.querySelector('nav');
-const html = document.documentElement;
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
-    }
-  });
+  if (window.scrollY > 0) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
